b9b8382d8f46223e98abb854c1eaebf4962f3f8d
zhengbinGitHub <122606861@qq.com>
2020-11-18 23:45:46 +0800

    str

--- src/ApplePackageManager.php
+++ src/ApplePackageManager.php
@@ -63,6 +63,22 @@ class ApplePackageManager
     }
 
     /**
+     * 生成随机字串
+     * @param number $length 长度，默认为16，最长为32字节
+     * @return string
+     */
+    public function getNonceStr($length = 16)
+    {
+        // 密码字符集，可任意添加你需要的字符
+        $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        $str = "";
+        for ($i = 0; $i < $length; $i++) {
+            $str .= $chars[mt_rand(0, strlen($chars) - 1)];
+        }
+        return $str;
+    }
+
+    /**
      * 获取签名
      * @param $appId
      * @return bool
