1aa25a9d3af7944ed909ece8d72b4554f8e89f58
zhengbinGitHub <122606861@qq.com>
2020-11-18 18:08:54 +0800

    bug

--- src/routes.php
+++ src/routes.php
@@ -5,13 +5,13 @@
  * Date: 2020-11-04
  * Time: 17:35
  */
-Route::group(['prefix' => 'apple', 'namespace' => 'CwApp\Controllers'], function () {
-    Route::get('auth', 'AuthTokenController@index');
+Route::group(['namespace' => 'CwApp\Controllers'], function () {
+    Route::get('apple/auth', 'AuthTokenController@index')->name('apple.auth');
     Route::group(['middleware'=>['web', 'cwapp.auth:'.config('cwapp.app_guard')]], function($router){
-        $router->get('show', 'AppleController@show');
+        $router->get('apple/show', 'AppleController@show')->name('apple.show');
         //配置多个应用
-        $router->get('create', 'AppleController@create');
+        $router->get('apple/create', 'AppleController@create')->name('apple.create');
         //保存
-        $router->post('store', 'AppleController@store');
+        $router->post('apple/store', 'AppleController@store')->name('apple.store');
     });
 });
