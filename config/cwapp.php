36f0fd43687a8ec61a4d0b44ba588f92518d4fb3
zhengbinGitHub <122606861@qq.com>
2020-12-01 17:11:02 +0800

    auth

--- config/cwapp.php
+++ config/cwapp.php
@@ -10,8 +10,8 @@ return [
     'app_guard' => env('APP_GUARD', ''),
     'app_default_platform' => env('APP_DEFAULT_PLATFORM', ''),
     'clients' => [
-        ['name' => '智慧门店开发信息', 'alias' => 'mall', 'sort' => env('APP_DEFAULT_CLIENT')=='mall'?1:0],
-        ['name' => '舒适到家开发信息', 'alias' => 'fuwu', 'sort' => env('APP_DEFAULT_CLIENT')=='fuwu'?1:0],
-        ['name' => '店酷云进销存开发信息', 'alias' => 'erp', 'sort' => env('APP_DEFAULT_CLIENT')=='erp'?1:0],
+        ['name' => '智慧门店开发信息', 'alias' => 'mall', 'sort' => env('APP_DEFAULT_PLATFORM')=='mall'?1:0],
+        ['name' => '舒适到家开发信息', 'alias' => 'fuwu', 'sort' => env('APP_DEFAULT_PLATFORM')=='fuwu'?1:0],
+        ['name' => '店酷云进销存开发信息', 'alias' => 'erp', 'sort' => env('APP_DEFAULT_PLATFORM')=='erp'?1:0],
     ],
 ];
