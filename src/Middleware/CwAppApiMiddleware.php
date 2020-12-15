bc0ae2db4cc162a6188631de3172bb7bbfd47e03
zhengbinGitHub <122606861@qq.com>
2020-12-15 13:14:32 +0800

    client

--- src/Middleware/CwAppApiMiddleware.php
+++ src/Middleware/CwAppApiMiddleware.php
@@ -32,7 +32,12 @@ class CwAppApiMiddleware
             return $this->returnMsg(0, '头部X-Auth-Platform／X-Auth-Appid参数为空');
         }
         try{
-            $this->validate($request->appid, $request->platform, $token);
+            $appInfo = ApiApp::query()->where(['app_id' => $request->appid, 'platform' => $request->platform])->first(['tenant_id', 'app_id', 'app_secret']);
+            if(!isset($appInfo->tenant_id)){
+                return $this->returnMsg(0, '应用信息为空');
+            }
+            $request->headers->set('tenant_id', $appInfo->tenant_id??0);
+            $this->validate($token, $appInfo->app_secret);
         }catch (\Exception $ex){
             return $this->returnMsg(0, $ex->getMessage());
         }
@@ -63,12 +68,11 @@ class CwAppApiMiddleware
 
     /**
      * 验证token
-     * @param $appId
-     * @param $platform
      * @param $token
+     * @param $appSecret
      * @throws \Exception
      */
-    public function validate($appId, $platform, $token)
+    public function validate($token, $appSecret)
     {
         /**** api传来的token ****/
         if(!isset($token) || empty($token))
@@ -80,7 +84,7 @@ class CwAppApiMiddleware
         if(!empty($explode[0]) && !empty($explode[1]) && !empty($explode[2]) && !empty($explode[3]) )
         {
             $info = $explode[0].'.'.$explode[1].'.'.$explode[2];//信息部分
-            $true_signature = hash_hmac('md5', $info, ApiApp::query()->where(['app_id' => $appId, 'platform' => $platform])->value('app_secret'));//正确的签名
+            $true_signature = hash_hmac('md5', $info, $appSecret);//正确的签名
             if ($true_signature != $explode[3])
             {
                 throw new \Exception('Token不合法', 400);
