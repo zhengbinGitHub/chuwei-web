d3759f9e1695292716bb9afa0467d594ebbafe94
zhengbinGitHub <122606861@qq.com>
2020-12-15 17:59:31 +0800

    bug

--- src/routes.php
+++ src/routes.php
@@ -5,7 +5,7 @@
  * Date: 2020-11-04
  * Time: 17:35
  */
-Route::group(['namespace' => 'CwApp\Controllers'], function () {
+Route::group(['namespace' => 'ChuWei\Client\Web\Controllers'], function () {
     Route::group(['middleware'=>['web', 'cwapp.auth:'.config('cwapp.app_guard')]], function($router){
         //配置多个应用
         $router->get('proxy/client/{merchant_id}', 'ProxyController@client')->name('proxy.client');
